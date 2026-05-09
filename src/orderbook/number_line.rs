48ce400e0904f9e32896d96f6f9287e77d47df20
Samuel Burac <samuelburac@gmail.com>
2026-05-09 19:16:36 -0400

    some do

--- src/orderbook/number_line.rs
+++ src/orderbook/number_line.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::collections::VecDeque;
+use std::ops::Index;
 use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
@@ -17,6 +18,8 @@ pub struct LineItemQueue {
     prev_queue: RefCell<Weak<LineItemQueue>>,
 }
 
+const DEFAULT_INIT_CAPACITY: usize = 100;
+
 /** A numberline which is a list of lists
 * Each entry in the list represents a price.
 * Each price point has a list that acts as a queue.
@@ -30,6 +33,7 @@ pub struct NumberLine {
     min: u64,
     items: Vec<Option<Rc<LineItemQueue>>>,
     size: usize,
+    capacity: usize,
 }
 
 impl LineItem {
@@ -42,6 +46,17 @@ impl LineItem {
 }
 
 impl LineItemQueue {
+    pub fn new(line_item: LineItem) -> LineItemQueue {
+        let res = LineItemQueue {
+            value: line_item.value,
+            queue: RefCell::new(VecDeque::new()),
+            next_queue: RefCell::new(Weak::new()),
+            prev_queue: RefCell::new(Weak::new()),
+        };
+        res.queue.borrow_mut().push_front(line_item);
+        res
+    }
+
     pub fn add(&self, line_item: LineItem) {
         self.queue.borrow_mut().push_back(line_item);
     }
@@ -56,14 +71,30 @@ impl NumberLine {
         NumberLine {
             max: 0,
             min: 0,
-            items: vec![None; 50],
+            items: vec![None; DEFAULT_INIT_CAPACITY],
             size: 0,
+            capacity: DEFAULT_INIT_CAPACITY,
         }
     }
 
     pub fn add(&self, line_item: LineItem) {
-        let index = line_item.value % self.items.len() as u64;
+        let index = line_item.value % self.capacity as u64;
+
         // check index
+        if let Some(q) = self.items.index(index as usize) {
+            // if the queue is for the same dollar value add the item to the queue
+            if q.value == line_item.value {
+                q.add(line_item);
+            } else {
+                // TODO: resize map
+            }
+        } else {
+            let nouQ = LineItemQueue::new(line_item);
+            //TODO: find next and previous queues
+
+            // for now just iterate until hit next and prev
+        }
+
         // if index is none create queue with LineItem
         // add next and previous queue refs to queue
         //
